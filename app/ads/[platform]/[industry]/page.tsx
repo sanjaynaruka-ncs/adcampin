f0be589806ebfda6b403ed5ac01622435fb1f801
“sanjaynaruka-ncs” <sanjaynaruka@gmail.com>
2026-05-27 06:52:59 +0530

    Remove ISR from industry programmatic pages

--- app/ads/[platform]/[industry]/page.tsx
+++ app/ads/[platform]/[industry]/page.tsx
@@ -6,7 +6,6 @@ import SEOShareEmbed from "@/app/components/seo_share_embed";
 import Navbar from "../../../components/navbar";
 export const dynamicParams = true;
 export const dynamic = "force-static";
-export const revalidate = 86400;
 
 export const metadata = {
   robots: {
